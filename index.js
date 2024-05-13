e1d6a9eb7581563bc1b4c0be229bd32627615f38
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-05-13 11:43:18 +0500

    Sigma5.1

---
+++ index.js
@@ -0,0 +1,11 @@
+let son = Number(prompt("1 dan 7 gacha raqam kiriting: "));
+
+switch (son) {
+    case 1: alert("Dushanba"); break;
+    case 2: alert("Seshanba"); break;
+    case 3: alert("Сhorshanba"); break;
+    case 4: alert("Payshanba"); break;
+    case 5: alert("Juma"); break;
+    case 6: alert("Shanba"); break;
+    case 7: alert("Yakshanba"); break;
+} 
